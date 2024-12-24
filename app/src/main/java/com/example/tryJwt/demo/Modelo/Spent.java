d5e746562a05056e42963a2076d36abcc2edc2bf
Sebastián Butcovich <sebabutcovich@gmail.com>
2024-12-24 11:05:25 -0300

    Back deploy 09

---
+++ app/src/main/java/com/example/tryJwt/demo/Modelo/Spent.java
@@ -0,0 +1,26 @@
+package com.example.tryJwt.demo.Modelo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "spent")
+public class Spent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "descripción")
+    private String descripcion;
+    @Column(name = "fecha")
+    private Date fecha;
+
+}
